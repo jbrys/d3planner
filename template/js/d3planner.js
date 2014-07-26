5ea3345eda5b30c0420869cde4abd59564042504
Sean Seymour <seanseymour@gmail.com>
2014-07-25 22:43:46 -0700

    set bonuses and damage calculation and it's grrrrreat.

--- template/js/d3planner.js
+++ template/js/d3planner.js
@@ -9,3 +9,29 @@ if (!String.prototype.format) {
     });
   };
 }
+
+function get_stat_color(min,max,val){
+    return percent_to_color((val-min)/(max-min));
+}
+
+function percent_to_color(percent){
+    R=Math.ceil(255*(1-percent));
+    G=Math.floor(255*percent);
+    B=parseInt(0);
+
+    R=R.toString(16);
+    G=G.toString(16);
+    B=B.toString(16);
+
+    while(R.length < 2){
+        R = "0"+R;
+    }
+    while(G.length < 2){
+        G = "0"+G;
+    }
+    while(B.length < 2){
+        B = "0"+B;
+    }
+
+    return "#"+R+G+B;
+}
