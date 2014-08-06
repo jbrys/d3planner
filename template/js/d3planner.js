6adfeb3c164922a7c92cfc931d04d5e145be2291
Sean Seymour <seanseymour@gmail.com>
2014-08-06 13:40:48 -0700

    initial skills

--- template/js/d3planner.js
+++ template/js/d3planner.js
@@ -17,14 +17,13 @@ if (!Array.prototype.max) {
 }
 
 function save_to_cookie(char){
-    $.each(char, function(slot, data){
-        $.cookie('d3planner_'+slot, data);
+    $.each(_cookies, function(i, slot){
+        $.cookie('d3planner_'+slot, char[slot]);
     });
 }
 
 function load_from_cookie(char){
-    $(".slot").each(function(){
-        var slot = $(this).attr("id");
+    $.each(_cookies, function(i, slot){
         var cookie_name = 'd3planner_'+slot;
         if($.cookie(cookie_name))
             char[slot] = $.cookie(cookie_name);
@@ -131,6 +130,7 @@ function get_display_value(val, scale){
 }
 
 function get_template(affix, value){
+	affix = get_char_affix(affix);
     affix_stuff = _translate[affix] || {};
     var output;
     if (value){
@@ -143,6 +143,10 @@ function get_template(affix, value){
     	output = _translate.tooltip.template.format(output,affix_stuff.tooltip);
     }
     return output;
+}
+
+function get_char_affix(affix){
+	return _classes[_char.settings.class].translate[affix] || affix;
 }
 
 function clear_slot(item_ddl){
@@ -163,6 +167,7 @@ function load_slot(cat, item_ddl, group){
 
         $.each(data.items, function(key, item) {
             item.cat = cat;
+            item.base = data.base || cat;
             parent_item.append($("<option></option>").html(key));
         });
 
@@ -175,21 +180,33 @@ function load_slot(cat, item_ddl, group){
 }
 function load_ddl(item_ddl){
     clear_slot(item_ddl);
-    $.each(item_ddl.attr("class").split(/\s+/), function(i, slot){
-        if($.inArray(slot, _valid_slots) > -1){
-            load_slot(slot, item_ddl, false);
+    var slot = item_ddl.parents(".slot").prop("id");
+
+	if( _char.settings.class && _classes[_char.settings.class] &&
+		_classes[_char.settings.class].class_specific_slots &&
+		_classes[_char.settings.class].class_specific_slots[slot]){
+		$.each(_classes[_char.settings.class].class_specific_slots[slot], function(i, item){
+			load_slot(item, item_ddl, false);
+		});
+    }
+
+    $.each(item_ddl.attr("class").split(/\s+/), function(i, cat){
+        if($.inArray(cat, _valid_slots) > -1){
+            load_slot(cat, item_ddl, false);
         }
     });
     item_ddl.parent().children(".chosen-select").trigger("chosen:updated");
 }
 
 function join_item(item){
+	debug(JSON.stringify(item));
     if(item && !item.joined){
-        $.each(_affix.primary[item.cat], function(stat,value){
+    	var primary_affixes = _affix.primary[item.base];
+        $.each(primary_affixes, function(stat,value){
             if(item.primary)
                 item.primary[stat] = value;
         });
-        item.socket_value = _affix.primary[item.cat].socket_value;
+        item.socket_value = primary_affixes.socket_value;
         item.joined = true;
     }
 }
@@ -216,4 +233,125 @@ function item_change(){
         });
     }
     $("#"+slot+ " .chosen-select").trigger("chosen:updated");
+}
+
+function import_begin(){
+	$(".import #character_list, .import #import_end").hide().empty();
+	$(".import").show();
+}
+function fetch(){
+	var matches = /([a-z]+)[#\-](\d{4})/i.exec($(".import #bid").val());
+	if(matches && matches.length == 3){
+		var profile_url = _base+"bnet_profile/{0}/{1}/".format(matches[1],matches[2])
+		$.getJSON(profile_url, function(data){
+			$.each(data.heroes, function(i, hero){
+				$("#character_list")
+					.append("<option value='{0}'>{1}</option>".format(hero.id,hero.name));
+			});
+			$(".import #character_list, .import #import_end").show();
+		});
+	}
+}
+function import_end(){
+	$(".import").hide();
+	var matches = /([a-z]+)[#\-](\d{4})/i.exec($(".import #bid").val());
+	var hero_id = $("#character_list").val();
+	var character_profile_url = _base+"character_profile/{0}/{1}/{2}".format(matches[1],matches[2],hero_id)
+	$.getJSON(character_profile_url, function(c_data){
+		_char.settings = {};
+		_char.settings.class = _bnet_translate[c_data.class];
+
+		$.each(c_data.items, function(slot, item){
+			slot = _bnet_translate[slot] || slot;
+			var item_name =  _bnet_translate[item.name] || item.name;
+			debug(JSON.stringify(slot));
+			item_blob = _slots[slot].items[item_name];
+
+			_char[slot] = {};
+			if(item_blob){
+				join_item(item_blob);
+				_char[slot].name = item_name;
+				_char[slot].affixes = {};
+				_char[slot].affixes.primary = {};
+				var item_url = _base+"item_details/{0}".format(item.tooltipParams.replace("item/",""));
+				$.getJSON(item_url, function(i_data){
+					$.each(i_data.attributesRaw, function(key,value){
+						var value = (value.min+value.max)/2;
+						translate_data = _bnet_translate[key.split("#")[0]] || _bnet_translate[key];
+						if(translate_data){
+							var scale = translate_data.scale || 0;
+							affixes = translate_data.name;
+							value = value*Math.pow(10,scale);
+							$.each(affixes, function(i, affix){
+								if(item_blob.primary[affix]){
+									if(!_char[slot].affixes.primary[affix]){
+										_char[slot].affixes.primary[affix] = $.extend(true, {},item_blob.primary[affix]);
+									}
+									scale = _char[slot].affixes.primary[affix].scale || 0;
+									value = Math.round(value*Math.pow(10,scale));
+									if(affix == "max_damage"||affix == "min_damage"){
+										debug([slot,affix,value,_char[slot].affixes.primary[affix].val]);
+										debug(JSON.stringify(i_data.attributesRaw[key]));
+									}
+									_char[slot].affixes.primary[affix].val =
+										(_char[slot].affixes.primary[affix].val || 0) + value
+								}
+							});
+						}
+						else{
+
+						}
+					});
+				});
+				debug("-----");
+			}
+		});
+		save_to_cookie(_char);
+		update_char();
+	});
+	$(".import").hide();
+}
+
+function simplify(statement, values){
+	debug(["statement",JSON.stringify(statement)]);
+	if(typeof(statement) == typeof({}))
+	{
+		var keys = Object.keys(statement);
+		for(var i = 0; i < keys.length; i++) {
+			var key = Object.keys(statement)[i];
+			if(values[key]){
+				if(statement[key]){
+					values[key] = simplify(statement[key], values);
+					debug(JSON.stringify(values));
+				}
+			}
+			else{
+				if (key == "add"){
+					var return_value = 0;
+					$.each(statement[key], function(i, summand){
+						return_value += simplify(summand, values)
+					});
+					return return_value;
+				}
+				else if (key == "multiply"){
+					var return_value = statement[key].length > 0 ? 1 : 0;
+					$.each(statement[key], function(i, summand){
+						return_value *= simplify(summand, values)
+					});
+					return return_value;
+				}
+			}
+		}
+	}
+	else if(typeof(statement) == typeof(0)){
+		return statement;
+	}
+	else if(typeof(statement) == typeof("")){
+		return values[statement]
+	}
+}
+
+function IsNumeric(input)
+{
+   return (input - 0) == input && input.length > 0;
 }
